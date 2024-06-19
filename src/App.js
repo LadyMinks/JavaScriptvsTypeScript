76bf029721c8e745dda913fcf9e3f132b003ccba
LadyMinks <minka-96@outlook.com>
2024-06-19 20:43:03 +0200

    setting up grocery list in JS. Can now add and remove items from list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {v4 as uuidv4} from "uuid";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [item, setItem] = useState("");
+    const [items, setItems] = useState([]);
+    const add = (e) => {
+        e.preventDefault();
+        if (!item) {
+            return;
+        }
+        setItems((items) => [
+            ...items,
+            {
+                id: uuidv4(),
+                item,
+                checked: false
+            }
+        ]);
+    };
+
+    const remove = (index) => {
+        setItems((items) => items.filter((_, i) => i !== index));
+    };
+    
+    return (
+        <div className="App">
+            <form onSubmit={add}>
+                <h1>Grocery List</h1>
+                <label>item</label>
+                <input value={item} onChange={(e) => setItem(e.target.value)}/>
+                <button type="submit">add</button>
+            </form>
+            {items.map((item, index) => {
+                return (
+                    <div className="d-flex flex-row mb-3 ListFlex" key={item.id}>
+                        <p>{item.item}</p>
+                        <button className="Button"
+                                onClick={() => remove(index)}>
+                            X
+                        </button>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
-
-export default App;
