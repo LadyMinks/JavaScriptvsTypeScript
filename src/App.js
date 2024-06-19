08d9fca579546c8168ea895b38d0156d4912cdf2
LadyMinks <minka-96@outlook.com>
2024-06-19 23:06:32 +0200

    Added extra features to grocery list.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,85 @@
-import React, {useState} from "react";
-import {v4 as uuidv4} from "uuid";
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./Item";
+import { v4 as uuidv4 } from "uuid";
 
-export default function App() {
+function App() {
     const [item, setItem] = useState("");
-    const [items, setItems] = useState([]);
-    const add = (e) => {
-        e.preventDefault();
-        if (!item) {
-            return;
+    const [edit, setEdit] = useState(false);
+    const [editId, setEditId] = useState();
+    const [list, setList] = useState([]);
+
+    const handleSubmit = (index) => {
+        const newItem = {
+            id: uuidv4(),
+            item: item,
+            complete: false,
+        };
+        index.preventDefault();
+        if (item && item.length <= 25 && !edit) {
+            setList([...list, newItem]);
+            setItem("");
+        } else if (item && item.length <= 25 && edit && editId) {
+            setList(
+                list.map((el) => {
+                    if (el.id === editId) {
+                        return { ...el, item: item };
+                    }
+                    return el;
+                })
+            );
+            setItem("");
+            setEditId(null);
+            setEdit(false);
         }
-        setItems((items) => [
-            ...items,
-            {
-                id: uuidv4(),
-                item,
-                checked: false
-            }
-        ]);
     };
 
-    const remove = (index) => {
-        setItems((items) => items.filter((_, i) => i !== index));
+    React.useEffect(() => {
+        localStorage.setItem("data", JSON.stringify(list));
+    }, [list]);
+
+    const handleChange = (e) => {
+        setItem(e.target.value);
     };
-    
+
     return (
         <div className="App">
-            <form onSubmit={add}>
-                <h1>Grocery List</h1>
-                <label>item</label>
-                <input value={item} onChange={(e) => setItem(e.target.value)}/>
-                <button type="submit">add</button>
+            <h1>Grocery List</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    className="input"
+                    type="text"
+                    value={item}
+                    placeholder="Enter the items"
+                    onChange={handleChange}
+                />
+                {edit ? (
+                    <button className="btn" type="submit">
+                        Edit Item
+                    </button>
+                ) : (
+                    <button className="btn" type="submit">
+                        Add Item
+                    </button>
+                )}
             </form>
-            {items.map((item, index) => {
-                return (
-                    <div className="d-flex flex-row mb-3 ListFlex" key={item.id}>
-                        <p>{item.item}</p>
-                        <button className="Button"
-                                onClick={() => remove(index)}>
-                            X
-                        </button>
-                    </div>
-                );
-            })}
+            <div>
+                {list.map((e, id) => (
+                    <Item
+                        key={id}
+                        id={e.id}
+                        item={e.item}
+                        list={list}
+                        setList={setList}
+                        complete={e.complete}
+                        setItem={setItem}
+                        setEdit={setEdit}
+                        setEditId={setEditId}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
+
+export default App;
---
+++ src/Item.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./Item.css";
+
+const Item = ({
+                  id,
+                  item,
+                  list,
+                  setEdit,
+                  setEditId,
+                  setItem,
+                  setList,
+                  complete,
+              }) => {
+    //Delete Item
+    const remove = (id) => {
+        setList(list.filter((el) => el.id !== id));
+    };
+
+    //Mark Item completed
+    const handleComplete = (id) => {
+        setList(
+            list.map((item) => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        complete: !item.complete,
+                    };
+                }
+                return item;
+            })
+        );
+    };
+
+    //Edit Item
+    const handleItem = (id) => {
+        const editItem = list.find((el) => el.id === id);
+        setItem(editItem.item);
+        setEdit(true);
+        setEditId(id);
+    };
+
+    return (
+        <div className="item">
+            <input
+                type="text"
+                value={item}
+                style={{
+                    border: "none",
+                    outline: "none",
+                    backgroundColor: "transparent",
+                    color: "black",
+                    fontSize: "20px",
+                }}
+                className={complete ? "complete" : ""}
+            />
+
+            <button
+                onClick={() => {
+                    const confirmBox = window.confirm("Do you want to edit this item?");
+                    if (confirmBox === true) {
+                        handleItem(id);
+                    }
+                }}>
+                edit
+            </button>
+            <button
+                onClick={() => handleComplete(id)}>
+                check
+            </button>
+
+            <button
+                onClick={() => {
+                    const confirmBox = window.confirm(
+                        "Are you sure you want to delete this item?"
+                    );
+                    if (confirmBox === true) {
+                        remove(id);
+                    }
+                }}>
+                remove
+            </button>
+        </div>
+    );
+};
+export default Item;
